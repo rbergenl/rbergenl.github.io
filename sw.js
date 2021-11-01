5f18230ff6810cffff9ab4843c26ef71fd916ca7
Robin ten Berge <robin@rtbprojects.com>
2021-11-01 13:35:09 +0100

    Updates

--- sw.js
+++ sw.js
@@ -27,7 +27,7 @@ workbox.core.clientsClaim();
  */
 self.__precacheManifest = [
   {
-    "url": "webpack-runtime-2d741b3588e1ed8d0e80.js"
+    "url": "webpack-runtime-0e12e630c193dcd8634f.js"
   },
   {
     "url": "framework-cc7022c8d9ab50daf3c0.js"
@@ -37,7 +37,7 @@ self.__precacheManifest = [
   },
   {
     "url": "offline-plugin-app-shell-fallback/index.html",
-    "revision": "43a2a3cb46f31d93840ccdcb4a93462d"
+    "revision": "256964b5cb43bd03c091767aa757155f"
   },
   {
     "url": "component---cache-caches-gatsby-plugin-offline-app-shell-js-ca7625a901b217b3cc85.js"
